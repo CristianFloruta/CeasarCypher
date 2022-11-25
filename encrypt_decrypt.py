5892f248f616f58b56684400fd4d4dfde66b492b
Cristian Floruta <115248323+CristianFloruta@users.noreply.github.com>
2022-11-25 15:27:29 +0000

    decypher added

---
+++ encrypt_decrypt.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+    return cipher_text
+
+
+def decrypt(plain_text, shift_amount):
+  decipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    decipher_text += alphabet[new_position]
+  print(f"The decoded text is {decipher_text}")
--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-from encrypt import encrypt
-from decrypt import decrypt
+from encrypt_decrypt import encrypt, decrypt
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+coding_messages_on = True
 
-encrypted_message = encrypt(plain_text = text, shift_amount = shift)
-print(f' Your encrypted code is: {encrypted_message} \n')
+while coding_messages_on:
 
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  
+  if direction == "encode":
+    
+    text = input("Type your message:\n").lower()
+    
+    shift = int(input("Type the shift number:\n"))
+  
+    encrypt(plain_text=text, shift_amount=shift)
 
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    play_on = input("Type 'yes' to continue, type 'no' to exit:\n").lower()
 
-decripted_message = decrypt(encoded_text = text, shift_amount = shift)
-print(f' Your decrypted code is: {decripted_message} ')
+    if play_on == "no":
 
+      coding_messages_on = False
 
+    else:
 
+      pass
 
+  elif direction == "decode":
 
+    text = input("Type your message:\n").lower()
+    
+    shift = int(input("Type the shift number:\n"))
+
+    decrypt(plain_text=text, shift_amount=shift)
+
+    play_on = input("Type 'yes' to continue, type 'no' to exit:\n").lower()
+
+    if play_on == "no":
+
+      coding_messages_on = False
+
+    else:
+
+      pass
